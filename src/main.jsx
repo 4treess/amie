1a0fcb0243fdd8c31f9dc467a05c2d895df26913
4treess <145797309+4treess@users.noreply.github.com>
2026-07-26 23:43:58 -0700

    step 2 of adding mines minigame part 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css' // Ensure this exists for your Tailwind or custom CSS
 import RelationshipTimeline from './App.jsx'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
