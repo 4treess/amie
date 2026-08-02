0c66e7b32f17a72e1d779d453d01518706837454
4treess <145797309+4treess@users.noreply.github.com>
2026-08-01 20:26:08 -0700

    Major visual changes that may not work

--- src/Mines.jsx
+++ src/Mines.jsx
@@ -516,9 +516,29 @@ const Mines = () => {
             </div>
 
             {/* SCORE / STATUS DISPLAY */}
-            <div className="bg-slate-50 p-4 rounded-xl flex items-center justify-between border border-slate-100 text-rose-500">
-              {activeTab === 'multiplayer'? nickname : "Your Stats:"}
-              <div className="flex items-center gap-2 text-xs text-slate-500 font-medium">
+            
+            <div className="bg-slate-50 p-4 rounded-xl flex items-center justify-between border border-slate-100 text-rose-500 text-xs">
+              {activeTab === 'multiplayer'? Object.values(playersList).sort((a, b) => a.score - b.score).map(({nickname, status, score}) => {
+                <div>{nickname}
+                  <div className="flex items-center gap-2 text-xs text-slate-500 font-medium">
+                    Status:
+                  </div>
+                  <span className="text-xs font-bold text-rose-500 uppercase tracking-wider">
+                    {status}
+                  </span>
+                  <div className={`${roundsCount === 0 ? "absolute text-transparent" : "flex items-center gap-2 text-xs text-slate-500 font-medium" }`}>
+                    Round:
+                  </div>
+                  <span className={`${roundsCount === 0 ? "absolute text-transparent" : "text-xs font-bold text-rose-500 uppercase tracking-wider"}`}>
+                    {currentRound} / {roundsCount}
+                  </span>
+                  <div className="flex items-center gap-2 text-xs text-slate-500 font-medium">
+                    Points:
+                  </div>
+                  <span className="text-xs font-bold text-rose-500 uppercase tracking-wider">
+                    {Math.round(score*100)/100}
+                  </span>
+                </div>}) : <span>"Your Stats:" <div className="flex items-center gap-2 text-xs text-slate-500 font-medium">
                 Status:
               </div>
               <span className="text-xs font-bold text-rose-500 uppercase tracking-wider">
@@ -536,7 +556,13 @@ const Mines = () => {
               <span className="text-xs font-bold text-rose-500 uppercase tracking-wider">
                 {Math.round(points*100)/100}
               </span>
+              </span>}
+            </div> 
+              
+            <div className="bg-slate-50 p-4 rounded-xl flex items-center justify-between border border-slate-100 text-rose-500 text-xs">
+              
             </div>
+            
           </>
         )}
 
